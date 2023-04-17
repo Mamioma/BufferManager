00ce4576b6b1e5af9532fa0d2441f757c0e73b56
m7ma <m7ma@ieng6-203.ucsd.edu>
2023-04-17 15:33:46 -0700

    secafter make doc

--- docs/search/functions_63.js
+++ docs/search/functions_63.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['clear',['clear',['../structbadgerdb_1_1_buf_stats.html#a04a848e5458491fd3ea56133464cadbd',1,'badgerdb::BufStats']]],
-  ['clearbufstats',['clearBufStats',['../classbadgerdb_1_1_buf_mgr.html#a702a264ae946b334414e51700edd81a0',1,'badgerdb::BufMgr']]],
+  ['clearbufstats',['clearBufStats',['../classbadgerdb_1_1_page_buffer_manager.html#abd40d11727f9bc9028c096d30a989c10',1,'badgerdb::PageBufferManager']]],
   ['create',['create',['../classbadgerdb_1_1_file.html#a1fb708b45103a606f189850d6bf83a0c',1,'badgerdb::File']]]
 ];
